5401ad758d7bc39818cff526cbf21f6739565a14
220100 <220100kd@yse-c.net>
2023-04-27 11:46:50 +0900

    完成

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -8,7 +8,7 @@ public class TitleManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerPrefs.DeleteAll();
     }
 
     // Update is called once per frame
